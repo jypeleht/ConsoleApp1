09e1ee9c634ca8d2f93f9b7a6c9c4d5ee8e5afa4
JohannaP <55647359+JohPak@users.noreply.github.com>
2019-11-28 18:14:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            
+            
+            Console.WriteLine(@"  _____                ");
+            Console.WriteLine(@" |_   _|__ _ ___ _____");
+            Console.WriteLine(@"   | |/ -_) '_\ V / -_)");
+            Console.WriteLine(@"   |_|\___|_|  \_/\___|");
+
+
         }
     }
 }
