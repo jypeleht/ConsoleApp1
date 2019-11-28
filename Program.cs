47bb8063019b831e31f5fd88fabcb1d9eece938e
ToniPoola <57809080+ToniPoola@users.noreply.github.com>
2019-11-28 17:53:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            //isot alkukirjaimet + !-merkki
+            Console.WriteLine("Hello World!");
         }
     }
 }
