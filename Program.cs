5d37d200089f9115f176116798330514038fb308
juhanasoini <juhanasoini@gmail.com>
2019-11-28 17:58:53 +0200

    Uuden tiedoston luonti.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string filename = @"tiedosto.txt";
+
             Console.WriteLine("Hello world");
+            if (!File.Exists(filename))
+                File.WriteAllText(filename, "Eka rivi\n");
+
+
+            File.AppendAllText(filename, "Seuraava rivi¨\n");
         }
     }
 }
